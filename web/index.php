b0fafb230b53664dbe2cc154b309ac9c016235c2
Maxime Jacquet SFDC <mjacquet@salesforce.com>
2015-12-14 18:33:44 +0100

    style form 1

--- web/index.php
+++ web/index.php
@@ -1,15 +1,30 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="/node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css">
+</head>
+<body>
+
 <form enctype="multipart/form-data" action="translate.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Upload the XML from site.com <input name="thefile" type="file" />
+    Upload the XML from site.com <input name="thefile" type="file" class="slds-button slds-button--brand"/>
     What is the base language of the file?
-    <select name="baseLanguage">
-      <option value="French">French</option>
-      <option value="English">English</option>
-    </select>
-    <input type="submit" value="Get French Translation" />
+
+    <div class="slds-form-element">
+      <label class="slds-form-element__label" for="selectSample1">Select Label</label>
+      <div class="slds-form-element__control">
+        <select id="baseLanguage" name="baseLanguage" class="slds-select">
+          <option>French</option>
+          <option>English</option>
+        </select>
+      </div>
+    </div>
+
+    <input type="submit" value="Get French Translation" class="slds-button slds-button--brand"/>
 </form>
+</body>
+</html>
 <?php
 /*
 require('../vendor/autoload.php');
