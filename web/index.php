9cb40b3b89d6310669b377f4699ef8d21f5ca9af
Maxime Jacquet SFDC <mjacquet@salesforce.com>
2015-12-15 13:25:53 +0100

    responsive tag

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+  <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" type="text/css" href="stylesheets/salesforce-lightning-design-system.css">
   <style>
   #uploadBtn {
