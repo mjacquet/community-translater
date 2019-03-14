8f8223acbb353790c08cc7621b664fc5665b4137
Max <github@jacquet.me>
2019-03-14 15:49:20 +0100

    Update footer.php

--- web/footer.php
+++ web/footer.php
@@ -1,8 +1,10 @@
  <?php
  header("Access-Control-Allow-Origin: *");?>
+
 <div id="block-bi-edito-bi-edito-footer-text" class="block block-bi-edito first last odd"> 
-  <div class="container">
-  <div class="edito-presentation">
+ <div class="border" style="width: 100%;height: 1px;border-bottom: 1px solid #ececec;margin-bottom: 20px;"></div>
+  <div class="container" >
+  <div class="edito-presentation" style="width: 940px;text-align: center;margin: auto;">
             <p><span style="font-size:10px">L'expertise de Bouygues Immobilier Logement<br>Choix attentif des matériaux, qualité architecturale, respect de l'environnement, Bouygues Immobilier Logement prend en considération tous les paramètres nécessaires à un confort optimal. L'intégralité des programmes d'immobilier neuf et d'investissement locatif de Bouygues Immobilier Logement font notamment l'objet d'une demande de certification RT2012 et NF Logement. Bouygues Immobilier développe les packs Flexom : solution de personnalisation de votre appartement et d’ouverture sur un logement connecté et intelligent. Bouygues Immobilier et La Centrale de Financement, une des plus grandes agences de courtier de France, unissent leurs compétences, et leur savoir-faire unique pour vous proposer les meilleures solutions possibles de crédit immobilier. Placer son épargne dans l'investissement locatif s'inscrit dans une démarche réfléchie. Bouygues Immobilier Logement propose ainsi un accompagnement personnalisé, afin de couvrir à 360° l'acquisition du bien immobilier. En résultat, 80%* des clients de Bouygues Immobilier Logement se disent prêts à recommander ce promoteur immobilier de premier plan.&nbsp;(*) Mesure réalisée par GNResearch, cabinet indépendant - données 2012.</span></p>      </div>
 </div>
 </div>
